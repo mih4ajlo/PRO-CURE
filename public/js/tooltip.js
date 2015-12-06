fbfcf2652880f8939b5b81e0ea75eb57f220c721
Mihajlo Andjelic <mihajlo88@gmail.com>
2015-12-06 05:00:57 +0100

    header

--- public/js/tooltip.js
+++ public/js/tooltip.js
@@ -1,27 +1,37 @@
 
-var myTool = d3.select("body")                    
-    .append("div")
-    .attr("class", "mytooltip")
-    .style("opacity", "0")
-    .style("display", "none");
+/*function napraviTT (argument) {
+     d3.select("body")                    
+      .append("div")
+      .attr("class", "mytooltip")
+      .style("opacity", "0")
+      .style("display", "none");
+
+      return d3.select(".mytooltip");
+}
 
 
-function dodajTT (d,k) {
-  
 
-  d3.select('.mytooltip')
+
+function dodajTT (d,k, coord) {
+  var temp = d3.select('.mytooltip');
+
+  if( temp.empty() ) temp = napraviTT();
+
+  temp
   .transition()
   .duration(500)
   .style("opacity", "1")                           
   .style("display", "block")
+  .style('left', coord(0) )
+  .style('top', coord(1) )
 
 }
 
-function skloniTT (d,j) {
+function skloniTT () {
   d3.select(this)
     .transition()
     .duration(500)
-    .attr("x", function(d) { return x(d.cocoa) - 20; })
+    //.attr("x", function(d) { return x(d.cocoa) - 20; })
     .style("cursor", "normal")
     .attr("width", 40)
     
@@ -33,10 +43,13 @@ function skloniTT (d,j) {
 
 
 function staviSadrzajTT (d,j, event) {
-
-  d3.select('.mytooltip').html("<div id='thumbnail'><span>" + d.name + "</span><img src='" + d.image + "'/></div>")
+  var narucilac = "" ;
+  if(d.narucilac != undefined) narucilac =  d.narucilac
+//<img src='" + d.image + "'/>
+  d3.select('.mytooltip').html("<div id='thumbnail'><span>" + d.name + "</span><br /> <div> 2014"+
+    .narucilac+"</div> </div>")
     .style("left", (d3.event.pageX - 113) + "px")   
-    .style("top", (d3.event.pageY - 190) + "px")
+    .style("top", (d3.event.pageY - 80) + "px")
     .on("mouseout", skloniTT )
 
-}
+}*/
